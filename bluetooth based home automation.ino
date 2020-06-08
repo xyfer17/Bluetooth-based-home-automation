d50908151e1f6cfd44983d37c6efc627a6c7c9d0
xyfer <nav3071999@gmail,com>
2020-06-08 21:38:53 +0530

    blutooth based home automation

---
+++ bluetooth based home automation.ino
@@ -0,0 +1,29 @@
+char junk;
+String inputString="";
+
+void setup()                    // run once, when the sketch starts
+{
+ Serial.begin(9600);            // set the baud rate to 9600, same should be of your Serial Monitor
+ pinMode(8, OUTPUT);
+  digitalWrite(8, LOW);
+}
+
+void loop()
+{
+  if(Serial.available()){
+  while(Serial.available())
+    {
+      char inChar = (char)Serial.read(); //read the input
+      inputString += inChar;        //make a string of the characters coming on serial
+    }
+    Serial.println(inputString);
+    while (Serial.available() > 0)
+    { junk = Serial.read() ; }      // clear the serial buffer
+    if(inputString == "a"){         //in case of 'a' turn the LED on
+      digitalWrite(8, HIGH);
+    }else if(inputString == "b"){   //incase of 'b' turn the LED off
+      digitalWrite(8, LOW);
+    }
+    inputString = "";
+  }
+}
---
+++ read.md
@@ -0,0 +1,4 @@
+# Bluetooth based home automation
+
+In the above  project we have develope home automation  system using
+arduino uno and Bluetooth HC05 module .
